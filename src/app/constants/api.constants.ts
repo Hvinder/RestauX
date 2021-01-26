8404088d54f0ecb56c12a04260365f42b3b8d7c7
Hvinder <harvinder.chavan7@gmail.com>
2021-01-26 18:49:02 +0530

    Added deployed server

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,5 +1,6 @@
 const port = "3000";
-const baseurl = `http://localhost:${port}`;
+// const baseurl = `http://localhost:${port}`;
+const baseurl = `https://safe-everglades-15846.herokuapp.com`;
 
 export const API_URLS = {
   places_endpoint: `${baseurl}/places`,
